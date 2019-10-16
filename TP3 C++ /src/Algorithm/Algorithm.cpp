fc40c18b45fc6db14405d982ea3d5ea743396700
Alexis Gardin <alexis.gardin@univ-cotedazur.fr>
2019-10-16 18:03:30 +0200

    add boost

---
+++ TP3 C++ /src/Algorithm/Algorithm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alexis Gardin on 16/10/2019.
+//
+
+#include "Algorithm.h"
---
+++ TP3 C++ /src/Algorithm/Algorithm.h
@@ -0,0 +1,14 @@
+//
+// Created by Alexis Gardin on 16/10/2019.
+//
+
+#ifndef GRAPH_ALGORITHM_H
+#define GRAPH_ALGORITHM_H
+
+
+class Algorithm {
+
+};
+
+
+#endif //GRAPH_ALGORITHM_H
